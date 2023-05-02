519e2dc7b27b42bdaa00370b385cc2c8393d8ade
MondSE <azirtuazon@gmail.com>
2023-05-02 14:12:00 +0800

    Added the Loading Page Animation Time-2:13PM Date-5/2/2023

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState , useEffect } from 'react';
 import "./App.css";
 import Header from './components/hearder/Header';
 import Home from './components/home/Home';
@@ -10,24 +10,34 @@ import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Scrollup from './components/scrollup/Scrollup';
 import Project from './components/project/Project';
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 const App = () => {
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    setloading(true)
+    setTimeout(() => {
+      setloading(false)
+    }, 5000)
+  },[]);
+
   return (
     <>
-      <Header />
-
-      <main className='name'>
-        <Home />
-        <About />
-        <Skills />
-        <Services />
-        <Qualification />
-        <Project />
-        <Contact />
-      </main>
-
-      <Footer />
-      <Scrollup />
+    {
+      loading ? (
+      <><PacmanLoader loading={loading} color={'#ffa500'} size={'40'} className='loading-screen' /><p className='loading-screen-text'>Welcome Visitor</p><p className='loading-screen-text2'>loading please wait a moment website</p></>
+      ): (     
+      <><Header /><main className='name'>
+            <Home />
+            <About />
+            <Skills />
+            <Services />
+            <Qualification />
+            <Project />
+            <Contact />
+          </main><Footer /><Scrollup /></>)
+    }
     </>
   )
 }
